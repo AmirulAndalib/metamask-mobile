0a9ad7cc84ece6bf527ea743ed10ddd3a9a49b45
micaelae <micaela.estabillo@consensys.net>
2026-09-17 15:03:39 -0700

    chore: remove unused QuickBuy mappings

--- app/components/UI/QuickBuy/quickBuy.ts
+++ app/components/UI/QuickBuy/quickBuy.ts
@@ -1,13 +1,5 @@
-import QuickBuyAmount from './QuickBuyAmount';
-import QuickBuyAmountScreen from './QuickBuyAmountScreen';
 import QuickBuyRoot from './QuickBuyRoot';
-import QuickBuyActionFooter from './components/QuickBuyActionFooter';
-import QuickBuyToolbar from './components/QuickBuyToolbar';
 
 export const QuickBuy = {
   Root: QuickBuyRoot,
-  AmountScreen: QuickBuyAmountScreen,
-  Toolbar: QuickBuyToolbar,
-  Amount: QuickBuyAmount,
-  Footer: QuickBuyActionFooter,
 } as const;
